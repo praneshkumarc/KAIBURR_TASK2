764ea219037e2eb186e55b1b32e28106b2b7605b
Pranesh Kumar C <121380063+praneshkumarc@users.noreply.github.com>
2025-10-18 20:54:44 +0530

    Delete src/main directory

--- src/main/java/com/example/taskrunner/exception/ApiError.java
+++
@@ -1,2 +0,0 @@
-package com.example.taskrunner.exception;
-public class ApiError { private final String message; public ApiError(String m){this.message=m;} public String getMessage(){return message;} }
