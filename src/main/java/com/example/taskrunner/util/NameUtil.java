d4bead90726e66969fde0d7ec5be3259c0ffb864
Pranesh Kumar C <121380063+praneshkumarc@users.noreply.github.com>
2025-10-19 09:56:25 +0530

    Delete src/main directory

--- src/main/java/com/example/taskrunner/util/NameUtil.java
+++
@@ -1,9 +0,0 @@
-// util/NameUtil.java
-package com.example.taskrunner.util;
-public class NameUtil {
-public static String toK8sName(String s){
-if (s==null) return "x";
-String cleaned = s.toLowerCase().replaceAll("[^a-z0-9-]","-").replaceAll("-+","-").replaceAll("(^-)|(-$)","");
-return cleaned.isBlank()?"x":cleaned;
-}
-}
